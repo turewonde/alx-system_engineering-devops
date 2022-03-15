0bd455502df589b63ae1c903542566d55672880c
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2022-03-15 00:48:13 -0700

    install neginx web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,6 @@
-# Install and configure nginx
-package { 'jfryman-nginx':
-  ensure => installed,
-}
-
-include nginx
-
-class { 'nginx':
-  manage_repo    => true,
-  package_source => 'nginx-stable',
-}
-
-nginx::resource::server { '34.73.76.135':
-  listen_port      => 80,
-  www_root         => '/var/www/html/',
-  vhost_cfg_append => { 'rewrite' => '^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent' },
-}
-
-file { 'index':
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Hello world!',
-}
+#!/usr/bin/env bash
+# Install Nginx web server (w/ Puppet)
+exec {'Install nginx':
+  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo echo "Holberton School" > /var/www/html/index.html && new_string="\\\trewrite ^/redirect_me https://www.youtube.com/ permanent;" && sudo sed -i "30i $new_string" /etc/nginx/sites-available/default && sudo service nginx start',
+  provider => shell,
+    }
